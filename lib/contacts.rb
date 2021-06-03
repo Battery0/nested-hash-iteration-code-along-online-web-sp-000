025f5f6b6a5a7a9c908bc277449968f6dd8b4b9e
Ian King <kickthechair@gmail.com>
2021-06-03 10:09:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,10 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name, contact_details_hash|
     if name == "Freddy Mercury"
-      contact_details_hash.each do |
-        
+      contact_details_hash.each do |attributes, data|
+        if attributes == :favorite_ice_cream_flavors
+          
+        end
       end
     end
   end
